5662dde610a067d861057f57cfe3cc3355da1593
djacoronel <djacoronel@gmail.com>
2018-01-02 13:05:02 +0800

    Groundwork.

--- app/src/main/java/com/djacoronel/lark/addeditcategory/AddEditActivity.kt
+++ app/src/main/java/com/djacoronel/lark/addeditcategory/AddEditActivity.kt
@@ -7,10 +7,7 @@ import android.content.DialogInterface
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import android.view.ViewGroup
+import android.view.*
 import com.djacoronel.lark.R
 import com.djacoronel.lark.ViewModelFactory
 import com.djacoronel.lark.addeditcategory.AddEditActivity_MembersInjector.create
@@ -18,6 +15,8 @@ import com.djacoronel.lark.databinding.ActivityAddEditBinding
 import com.djacoronel.lark.databinding.LayoutSetTimeBinding
 import com.djacoronel.lark.util.DateTimeUtil
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.layout_set_color.view.*
+import org.jetbrains.anko.alert
 import javax.inject.Inject
 
 
@@ -47,10 +46,11 @@ class AddEditActivity : AppCompatActivity() {
     private fun initMainBinding() {
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_add_edit)
         mainBinding.viewModel = viewModel
-        mainBinding.textViewSetTime.setOnClickListener { getSetTimeDialog().show() }
+        mainBinding.textViewSetTime.setOnClickListener { showSetTimeDialog() }
+        mainBinding.textViewSetColor.setOnClickListener { showSetColorDialog() }
     }
 
-    private fun getSetTimeDialog(): AlertDialog {
+    private fun showSetTimeDialog() {
         val intervals = resources.getStringArray(R.array.intervals)
         val dialogBinding: LayoutSetTimeBinding = DataBindingUtil.
                 inflate(LayoutInflater.from(this), R.layout.layout_set_time, null, false)
@@ -67,11 +67,39 @@ class AddEditActivity : AppCompatActivity() {
             viewModel.interval = DateTimeUtil.intervalToMillis(intervals[interval])
             viewModel.time = DateTimeUtil.hourMinuteToMillis(hourOfDay, minute)
         }
-        return AlertDialog.Builder(this)
+        AlertDialog.Builder(this)
                 .setView(dialogBinding.root)
                 .setPositiveButton("Set", onClickListener)
                 .setNegativeButton("Cancel", null)
                 .create()
+                .show()
+    }
+
+    private fun showSetColorDialog() {
+        val view = View.inflate(this, R.layout.layout_set_color, null)
+
+        alert {
+            title = "Pick Category Color"
+            customView = view
+            positiveButton("Reset") { viewModel.clearColor() }
+            negativeButton("Cancel") {}
+        }.show()
+
+        with(view) {
+            val colorCircles = listOf(
+                    color_circle_1, color_circle_2, color_circle_3, color_circle_4,
+                    color_circle_5, color_circle_6, color_circle_7, color_circle_8,
+                    color_circle_9, color_circle_10, color_circle_11, color_circle_12,
+                    color_circle_13, color_circle_14, color_circle_15, color_circle_16,
+                    color_circle_17, color_circle_18, color_circle_19, color_circle_20,
+                    color_circle_21, color_circle_22, color_circle_23, color_circle_24
+            )
+            for (colorCircle in colorCircles){
+                colorCircle.setOnClickListener {
+                    //TODO: implement code to set color in viewModel
+                }
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/djacoronel/lark/addeditcategory/AddEditViewModel.kt
+++ app/src/main/java/com/djacoronel/lark/addeditcategory/AddEditViewModel.kt
@@ -2,6 +2,7 @@ package com.djacoronel.lark.addeditcategory
 
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
 import com.djacoronel.lark.SingleLiveEvent
 import com.djacoronel.lark.data.model.Category
 import com.djacoronel.lark.data.model.Schedule
@@ -15,7 +16,7 @@ class AddEditViewModel(val categoryRepository: CategoryRepository) : ViewModel()
     internal var categoryAddedEvent = SingleLiveEvent<Void>()
 
     var id: Long = 0
-    var color: Int = 0
+    var color = ObservableField<String>("#FFFFFF")
     var label: String = ""
 
     var useInterval = ObservableBoolean(false)
@@ -25,14 +26,18 @@ class AddEditViewModel(val categoryRepository: CategoryRepository) : ViewModel()
     var schedule = Schedule()
     var ideas: List<Long> = listOf()
 
+    fun clearColor(){
+        color.set("#FFFFFF")
+    }
+
     fun loadCategory(categoryId: Long) {
         val category = categoryRepository.getCategory(categoryId)
         id = category.id
-        color = category.color
         label = category.label
         schedule = category.schedule
         ideas = category.ideas
 
+        color.set(category.color)
         useInterval.set(category.schedule.useInterval)
     }
 
@@ -42,7 +47,7 @@ class AddEditViewModel(val categoryRepository: CategoryRepository) : ViewModel()
         else schedule.time = time
 
         val category = Category()
-        category.color = color
+        category.color = color.get()
         category.label = label
         category.schedule = schedule
         category.ideas = ideas
